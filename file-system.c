82ea192b9a1c9f53d325fd7d0a61493c574e4099
jackbackrack <jrb@pobox.com>
2016-05-12 15:53:14 -0700

    use more robust dirent

---
+++ file-system.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <strings.h>
+#include <dirent.h>
+
+struct DirEntry {
+  int file_number;
+  int record_length;
+  char type;
+  char name_length;
+  char* name;
+};
+
+int readdirent (DIR* dir, struct DirEntry* res) {
+  struct dirent* ent = readdir(dir);
+  if (ent == NULL) {
+    return 0;
+  } else {
+    res->file_number = ent->d_ino;
+    res->record_length = ent->d_reclen;
+    res->type = ent->d_type;
+    res->name_length = strlen(ent->d_name);
+    res->name = ent->d_name;
+    return 1;
+  }
+}
