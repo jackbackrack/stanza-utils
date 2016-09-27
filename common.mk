f5ea152707757d22730f501c48f498f3431884e2
jackbackrack <jrb@pobox.com>
2016-09-27 16:04:22 -0700

    clean up

--- common.mk
+++ common.mk
@@ -11,14 +11,14 @@ OS2 := $(strip $(shell uname))
 ifeq ($(OS2), Darwin)
   $(info Compiling for Darwin)
   CFLAGS = -DMACOSX=1
-  FLAGS = $(COMMON_FLAGS) -lglfw3 -framework Carbon -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo
+  LIBS = $(COMMON_LIBS)
 else
   $(info Compiling for non Darwin)
   OS := $(strip $(shell uname -o))
   ifeq ($(OS), GNU/Linux)
     $(info Compiling for GNU/Linux)
     CFLAGS = 
-    FLAGS = $(COMMON_FLAGS) -lglfw -lrt -lm -lXrandr -lXinerama -lXi -lXcursor -lXrender -lGL -lGLU -lpthread -ldl -ldrm -lXdamage -lXfixes -lX11-xcb -lxcb-glx -lxcb-dri2 -lxcb-dri3 -lxcb-present -lxcb-randr -lxcb-xfixes -lxcb-render -lxcb-shape -lxcb-sync -lxshmfence -lXxf86vm -lXext -lX11 -lxcb -lXau -lXdmcp
+    LIBS = $(COMMON_LIBS) -lm
   endif
 endif
 
