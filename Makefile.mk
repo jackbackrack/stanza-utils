a05c1792d122690223d16a925f3f09e69d773388
jackbackrack <jrb@pobox.com>
2016-09-27 15:28:27 -0700

    add build include for others

---
+++ Makefile.mk
@@ -0,0 +1,18 @@
+${GEN}/eval.pkg: stanza-utils/eval.stanza 
+	stanza $< $(STZ_FLAGS) -pkg ${GEN}
+
+${GEN}/file-system.o: stanza-utils/file-system.c
+	cc $(CFLAGS) -c stanza-utils/file-system.c -o $@
+
+${GEN}/file-system.pkg: stanza-utils/file-system.stanza  utils.pkg 
+	stanza $< $(STZ_FLAGS) -pkg ${GEN}
+
+${GEN}/socket.o: stanza-utils/socket.c
+	cc $(CFLAGS) -c stanza-utils/socket.c -o $@
+
+${GEN}/socket.pkg: stanza-utils/socket.stanza ${GEN}/socket.o
+	stanza $< $(STZ_FLAGS) -pkg ${GEN}
+
+${GEN}/utils.pkg: stanza-utils/utils.stanza ${GEN}/file-system.o 
+	stanza $< $(STZ_FLAGS) -pkg ${GEN}
+
