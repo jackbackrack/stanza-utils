de1375d1745bc30ee3d3591144dd04e479156742
jackbackrack <jrb@pobox.com>
2017-10-03 20:40:34 -0700

    fix gen-repl to do proper overloading, and allow for ccfiles in dependencies

--- dependencies.mk
+++ dependencies.mk
@@ -1,5 +1,5 @@
 build/dependencies: stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES}
-	stanza stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES} -o build/dependencies
+	stanza stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES} -o build/dependencies ${DEPENDENCIES_SYNTAX_CC_FILES} # -optimize 
 
 build/dependencies.mk: build/dependencies
 	build/dependencies . > build/dependencies.mk
