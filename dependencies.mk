e913a50333a9ea269c122e7d53ecb481e7239015
jackbackrack <jrb@pobox.com>
2017-11-17 11:34:05 -0800

    change bootstrap to use boot directory and then support debug/release

--- dependencies.mk
+++ dependencies.mk
@@ -1,5 +1,5 @@
-build/dependencies: stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES}
-	stanza stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES} -o build/dependencies ${DEPENDENCIES_SYNTAX_CC_FILES} # -optimize 
+boot/dependencies: stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES}
+	stanza stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES} -o boot/dependencies ${DEPENDENCIES_SYNTAX_CC_FILES} # -optimize 
 
-build/dependencies.mk: build/dependencies
-	build/dependencies . > build/dependencies.mk
+boot/dependencies.mk: boot/dependencies
+	boot/dependencies . > boot/dependencies.mk
