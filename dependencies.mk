08f2aefdd7a0647afe3fede57293490c74c5c2b1
jackbackrack <jrb@pobox.com>
2017-11-20 10:45:33 -0800

    fix order of compilation

--- dependencies.mk
+++ dependencies.mk
@@ -1,5 +1,5 @@
-boot/dependencies: stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES}
-	stanza stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES} -o boot/dependencies ${DEPENDENCIES_SYNTAX_CC_FILES} # -optimize 
+boot/dependencies: stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES} 
+	stanza ${DEPENDENCIES_SYNTAX_FILES} stanza-utils/registry.stanza stanza-utils/dependencies.stanza -o boot/dependencies ${DEPENDENCIES_SYNTAX_CC_FILES} # -optimize 
 
 boot/dependencies.mk: boot/dependencies
 	boot/dependencies . > boot/dependencies.mk
