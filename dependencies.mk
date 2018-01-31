a161f0e018033a8e97b5f938f35b3737ab7eea2b
jackbackrack <jrb@pobox.com>
2018-01-31 11:04:32 -0800

    first version of docgen

--- dependencies.mk
+++ dependencies.mk
@@ -1,3 +1,6 @@
+boot/gendoc: stanza-utils/registry.stanza stanza-utils/gendoc.stanza ${DEPENDENCIES_SYNTAX_FILES} 
+	stanza ${DEPENDENCIES_SYNTAX_FILES} stanza-utils/registry.stanza stanza-utils/gendoc.stanza -o boot/gendoc ${DEPENDENCIES_SYNTAX_CC_FILES} # -optimize 
+
 boot/dependencies: stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES} 
 	stanza ${DEPENDENCIES_SYNTAX_FILES} stanza-utils/registry.stanza stanza-utils/dependencies.stanza -o boot/dependencies ${DEPENDENCIES_SYNTAX_CC_FILES} -optimize 
 
