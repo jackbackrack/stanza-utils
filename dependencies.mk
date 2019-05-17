346a2338f9079ee914b738f7b5280da06202c583
jackbackrack <jrb@pobox.com>
2019-05-17 13:34:23 -0700

    add support for new compiler

--- dependencies.mk
+++ dependencies.mk
@@ -1,8 +1,9 @@
-boot/gendoc: stanza-utils/registry.stanza stanza-utils/gendoc.stanza ${DEPENDENCIES_SYNTAX_FILES} 
-	stanza ${DEPENDENCIES_SYNTAX_FILES} stanza-utils/registry.stanza stanza-utils/gendoc.stanza -o boot/gendoc ${DEPENDENCIES_SYNTAX_CC_FILES} # -optimize 
+# boot/gendoc: stanza-utils/registry.stanza stanza-utils/gendoc.stanza ${DEPENDENCIES_SYNTAX_FILES} 
+# 	stanza ${DEPENDENCIES_SYNTAX_FILES} stanza-utils/registry.stanza stanza-utils/gendoc.stanza -o boot/gendoc ${DEPENDENCIES_SYNTAX_CC_FILES} # -optimize 
 
-boot/dependencies: stanza-utils/registry.stanza stanza-utils/dependencies.stanza ${DEPENDENCIES_SYNTAX_FILES} 
-	stanza ${DEPENDENCIES_SYNTAX_FILES} stanza-utils/registry.stanza stanza-utils/dependencies.stanza -o boot/dependencies ${DEPENDENCIES_SYNTAX_CC_FILES} -optimize 
+boot/dependencies: stanza-utils/registry.stanza stanza-utils/gen-makefile.stanza stanza-utils/read-dependencies.stanza 
+	stanza stanza-utils/registry.stanza stanza-utils/gen-makefile.stanza stanza-utils/read-dependencies.stanza -o boot/dependencies
 
 boot/dependencies.mk: boot/dependencies
-	boot/dependencies . > boot/dependencies.mk
+#	boot/dependencies . ${DEPS_STANZA}
+	boot/dependencies . ${DEPS_STANZA} > boot/dependencies.mk
