4afb4b2f347a275982ad0a01e7f89a64c41631cb
jackbackrack <jrb@pobox.com>
2019-07-29 15:40:35 -0700

    fix performance bug with efsw on macosx and default dir-name correctly

--- efsw.cpp
+++ efsw.cpp
@@ -53,7 +53,7 @@ class Watcher {
 Watcher* new_watcher (void) {
   Watcher* res = new Watcher();
   res->listener = new UpdateListener(&res->queue);
-  efsw::FileWatcher *fw = new efsw::FileWatcher( false );
+  efsw::FileWatcher *fw = new efsw::FileWatcher( true );
   res->watcher = fw;
   fw->followSymlinks( false );
   fw->allowOutOfScopeLinks( false );
