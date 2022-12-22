90ca1809f7d1d74e89e7cce407583312a7f8b557
jackbackrack <jrb@pobox.com>
2022-12-22 14:37:54 -0800

    merge jitx-client changes

--- utils-file-system.c
+++ utils-file-system.c
@@ -1,28 +1,7 @@
-// See license.txt for details about licensing.
+#ifdef PLATFORM_WINDOWS
+#include "utils-file-system-win32.c"
+#endif
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
-
-struct DirEntry {
-  int file_number;
-  int record_length;
-  char type;
-  char name_length;
-  char* name;
-};
-
-int readdirent (DIR* dir, struct DirEntry* res) {
-  struct dirent* ent = readdir(dir);
-  if (ent == NULL) {
-    return 0;
-  } else {
-    res->file_number = ent->d_ino;
-    res->record_length = ent->d_reclen;
-    res->type = ent->d_type;
-    res->name_length = strlen(ent->d_name);
-    res->name = ent->d_name;
-    return 1;
-  }
-}
+#if defined(PLATFORM_OS_X) || defined(PLATFORM_LINUX)
+#include "utils-file-system-posix.c"
+#endif
